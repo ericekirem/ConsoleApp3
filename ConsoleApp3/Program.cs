343ac4d616c27255582a68205a547f9588867435
ireme <ireme@IREM>
2024-07-24 17:13:10 +0300

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,24 @@
+﻿
+int yas;
+yas = 24; // assignment - set - atama işlemi.
+
+Console.WriteLine(yas); // read - okuma işlemi.
+
+char karakter = 'i'; // assignment          
+
+string isim = "irem"; //assignment
+
+// string aslında karakterlerden oluşan bir dizi.
+
+
+Console.WriteLine("Lütfen adınızı yazınız");
+isim = Console.ReadLine(); //assignment
+
+Console.WriteLine("Değişken içerisindeki veri ---->" + isim); // read
+
+Console.WriteLine("-----------");
+
+Console.WriteLine("Lütfen yaşınızı giriniz.");
+int yas2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Kişinin yaşı -> " + yas2);
